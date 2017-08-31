af1b95f6f2d70cb77e206d3a36b15aa3bb2a2696
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-08-31 09:11:36 -0500

    Update Comments

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -5,6 +5,9 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("GitHub Test!");
+
+//How do I link my work to my Google site?
+
 	}
 
 }
